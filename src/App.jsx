4a3d0776b8829adc9ec69af7bacd5f2ad0c151af
Alex <alexseeley134@gmail.com>
2025-01-05 01:05:24 -0700

    Remove header and add vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 data-testid="the-big-header">Our First Test</h1>
-
       <div
         className="flex justify-evenly bg-amber-300"
         data-testid="header-menu"
